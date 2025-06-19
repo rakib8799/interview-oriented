caa80fd4ad4f6016ac39352c007377d6cb826f68
rakib8799 <mkrakib007@gmail.com>
2025-06-19 22:16:43 +0600

    Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic Controller
+Route::get('/home', [HomeController::class, 'home']);
+Route::get('/about', [AboutController::class, 'about']);
+
+// Single action controller
+Route::get('/single-action-controller', SingleActionController::class);
+
+// Resource controller
+Route::resource('users', UserController::class);
+// Route::resource('users', UserController::class)->except(['show', 'destroy']);
+// Route::resource('users', UserController::class)->only(['show', 'destroy']);
